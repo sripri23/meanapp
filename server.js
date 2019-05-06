04f3aca95b0848e07abcbab5cefb5cdedafb1f7e
Priya <sripri23@gmail.com>
2019-05-06 16:11:10 -0400

    ignore

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ const app = express();
 app.use(express.static('static_files'));
 
 // start the server at URL: http://localhost:3000/
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000/');
+app.listen(5000, () => {
+  console.log('Server started at http://localhost:5000/');
 });
